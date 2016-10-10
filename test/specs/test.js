3c9945cfce17c93d3d7a4e5b1e1f3f35d7aa99c4
Andrew Crist <andrew.l.crist@gmail.com>
2016-10-09 20:15:02 -0600

    Please work

--- test/specs/test.js
+++ test/specs/test.js
@@ -13,6 +13,17 @@ describe('welcome page', function(){
     assert.equal(toDoBody.getValue(), 'great body');
   });
 
+  it('should clear the input fields on save', function(){
+    browser.url('/');
+    browser.click('.save-button');
+
+    var toDoTitle = browser.element('.title');
+    var toDoBody = browser.element('.body');
+
+    assert.equal(toDoTitle.getValue(), '');
+    assert.equal(toDoBody.getValue(), '');
+  });
+
   it('should be able to add todos to the page', function(){
     browser.url('/');
 
@@ -22,6 +33,17 @@ describe('welcome page', function(){
     toDoTitle.setValue('great title');
     toDoBody.setValue('great body');
     browser.click('.save-button');
+
+    var toDoTitles = browser.getText('.new-title-input');
+    var toDoBodys = browser.getText('.new-body-input');
+
+    assert.equal(toDoTitles, 'great title');
+    assert.equal(toDoBodys, 'great body');
+  });
+
+  it('should display the todos on refresh', function(){
+    browser.url('/');
+    browser.refresh();
 
     var toDoTitles = browser.getText('.new-title-input');
     var toDoBodys = browser.getText('.new-body-input');
