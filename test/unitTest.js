0f435a510abf4e96fb4739462652f9029deccb0f
Alex Pilewski <pilewski.alex@gmail.com>
2016-10-09 20:55:07 -0600

    ToDo unit testing

--- test/unitTest.js
+++ test/unitTest.js
@@ -1,7 +1,15 @@
 const assert = require('chai').assert;
-const AllToDos = require('../lib/AllToDos.js');
-require('../lib/scripts.js');
+const ToDo = require('../lib/todo.js');
 
 describe('ToDo Object Unit Tests', function(){
 
+  it('ToDo should be an object', function(){
+    var todo = new ToDo();
+    assert.isObject(todo, true);
+  });
+  
+  it('should have a default importance of normal', function(){
+    var todo = new ToDo();
+    assert.equal(todo.importance, 'Normal');
+  });
 });
